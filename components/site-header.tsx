79b67ff2da6db2d1be74b7f8f0f28dda3e963cfc
Zealeys <wesleypedrocastaldelli@gmail.com>
2024-09-09 17:09:57 -0300

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -21,17 +21,7 @@ export function SiteHeader({ children }: Props) {
 
   const links = (
     <nav className="flex items-center gap-4 md:gap-1">
-      <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
-        <div
-          className={buttonVariants({
-            size: "sm",
-            variant: "ghost",
-          })}
-        >
-          <Icons.gitHub className="size-5" />
-          <span className="sr-only">GitHub</span>
-        </div>
-      </Link>
+      
       <ThemeToggle />
     </nav>
   )
@@ -53,7 +43,7 @@ export function SiteHeader({ children }: Props) {
               />
             </div>
             <div className="hidden flex-1 items-center justify-end space-x-4 md:flex">
-              {links}
+              
             </div>
           </div>
         </div>
@@ -85,7 +75,7 @@ export function SiteHeader({ children }: Props) {
                 )
             )}
           </nav>
-          {links}
+          
         </div>
       </div>
       {/* Backdrop */}
