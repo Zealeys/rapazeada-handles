3791e1b84124d865275720c1af60b13a72ea5ebd
Zealeys <wesleypedrocastaldelli@gmail.com>
2024-09-10 03:31:31 -0300

    Update page.tsx

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -110,7 +110,7 @@ export default async function IndexPage({
     <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-4">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Utilize o domínio {domain} (Comunidade do Cellbit) <br className="hidden sm:inline" />
+          Utilize o domínio {domain} <br className="hidden sm:inline" />
           no seu nome de Usuário do Bluesky!
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
